db1d9a3cb4f2e07f9875bb4756facba20c2e6ac5
Binnette <binnette@gmail.com>
2020-03-22 18:19:45 +0100

    Script: add gueris in CSV

--- build.js
+++ build.js
@@ -84,6 +84,7 @@ function jsonToCsvRow(json) {
     deces: 'deces' in json ? json.deces : '',
     reanimation: 'reanimation' in json ? json.reanimation : '',
     hospitalises: 'hospitalises' in json ? json.hospitalises : '',
+    gueris: 'gueris' in json ? json.gueris : '',
     source_nom: (json.source && json.source.nom) || '',
     source_url: (json.source && json.source.url) || '',
     source_type: json.sourceType
