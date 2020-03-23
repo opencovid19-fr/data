da912002ba60dd91ce1b53d02fe665bc849c202a
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2020-03-23 16:48:28 +0100

    Move validation to validate.js

--- build.js
+++ build.js
@@ -3,9 +3,8 @@ const {join} = require('path')
 const {flatten, chain, omit} = require('lodash')
 const {outputJson} = require('fs-extra')
 const glob = require('glob')
-const {ensureArray, readYamlFile, outputCsv, stringCompare} = require('./lib/util')
-const departements = require('@etalab/decoupage-administratif/data/departements.json')
-const regions = require('@etalab/decoupage-administratif/data/regions.json')
+const {ensureArray, readYamlFile, outputCsv} = require('./lib/util')
+const validate = require('./lib/validate')
 
 const sources = [
   'agences-regionales-sante',
@@ -109,53 +108,6 @@ function showMetrics(rows) {
   console.log(`Nombre d’entrées avec le nombre d’hospitalisations : ${wHospitalises.length}`)
   console.log(`Nombre d’entrées avec le nombre de personnes guéries : ${wGueris.length}`)
   console.log(`Nombre d’entrées sans source : ${woSource.length}`)
-}
-
-function validate(flattenedData) {
-  let hasErrors = false
-
-  function notValid(row, errorMessage) {
-    console.error(`${row.sourceFile} | ${errorMessage}`)
-    hasErrors = true
-  }
-
-  flattenedData.forEach(row => {
-    if (!row.code) {
-      return notValid(row, 'Code non défini')
-    }
-
-    const [codeType, codeValue] = row.code.split('-')
-
-    if (!['REG', 'DEP', 'COM', 'FRA', 'WORLD'].includes(codeType)) {
-      return notValid(row, `Type de code non valide : ${codeType}`)
-    }
-
-    if (codeType === 'REG') {
-      const region = regions.find(r => r.code === codeValue)
-      if (!region) {
-        return notValid(row, `Code région inconnu : ${codeValue}`)
-      }
-
-      if (!stringCompare(region.nom, row.nom)) {
-        return notValid(row, `Incohérence entre le code région et le nom associé : ${region.nom} => ${row.nom}`)
-      }
-    }
-
-    if (codeType === 'DEP') {
-      const departement = departements.find(d => d.code === codeValue)
-      if (!departement) {
-        return notValid(row, `Code département inconnu : ${codeValue}`)
-      }
-
-      if (!stringCompare(departement.nom, row.nom)) {
-        return notValid(row, `Incohérence entre le code département et le nom associé : ${departement.nom} => ${row.nom}`)
-      }
-    }
-  })
-
-  if (hasErrors) {
-    throw new Error('Échec de validation')
-  }
 }
 
 async function main() {
